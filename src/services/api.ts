6b630f6b0185ef80ee9e1904740cb538cba834fe
jovic03 <42389786+jovic03@users.noreply.github.com>
2022-06-10 21:29:14 -0300

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
-import axios from "axios"
+import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'https://api-blueworking.herokuapp.com/'
+  baseURL: 'https://api-blueworking.herokuapp.com/'
 })
 
 export default api;
